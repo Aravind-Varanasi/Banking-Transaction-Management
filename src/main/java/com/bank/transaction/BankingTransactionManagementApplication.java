15dd968308bd10f1dbf7de56e7875318a6b25a8f
Aravind-Varanasi <157795620+Aravind-Varanasi@users.noreply.github.com>
2025-03-12 18:20:13 +0530

    Create BankingTransactionManagementApplication.java

---
+++ src/main/java/com/bank/transaction/BankingTransactionManagementApplication.java
@@ -0,0 +1,12 @@
+package com.bank.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingTransactionManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingTransactionManagementApplication.class, args);
+    }
+}
